4c2169ca74f0ba443a82cdf2416f1521da6125c8
ficorrea <felipecorrea@ficorrea.local>
2024-05-05 21:17:04 -0300

    add some files

--- geneticos/feature_selection/main.py
+++ geneticos/feature_selection/main.py
@@ -9,13 +9,15 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.metrics import ConfusionMatrixDisplay, RocCurveDisplay
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.svm import SVC
 
-FILENAME_COMPLEMENT = 'agg'
+DATASET = 'german_credit'
 
 # Load dataframe
-df = pd.read_csv(f'musk_{FILENAME_COMPLEMENT}.csv')
-cols = df.columns[2:-1]
-x = df.iloc[:, 2:-1]
+df = pd.read_csv(f'{DATASET}.csv')
+cols = df.columns[0:-1]
+x = df.iloc[:, 0:-1]
 y = df.iloc[:, -1]
 
 # Scaling dataframe
@@ -23,8 +25,12 @@ rob = MinMaxScaler()
 x = rob.fit_transform(x)
 x = pd.DataFrame(x, columns=cols)
 
-# Create model
-mdl = RandomForestClassifier(criterion='entropy', random_state=42, n_jobs=-1)
+# Create models
+random_state = 42
+models = [
+    (AdaBoostClassifier(random_state=random_state), 'AdaBoost'),
+    (RandomForestClassifier(random_state=random_state, n_jobs=-1), 'RandomForest'),
+    (SVC(random_state=random_state), 'SupportVectorMachine')]
 
 # Get columns by individual
 def get_cols(df, ind):
@@ -43,14 +49,14 @@ def plot_dash(values, selection, crossover):
     plt.title(f'{str.upper(selection)}.{str.upper(crossover)}')
     plt.xlabel('Generation')
     plt.ylabel('Score')
-    plt.savefig(f'{selection}.{crossover}.{FILENAME_COMPLEMENT}.png')
+    plt.savefig(f'{selection}.{crossover}.{DATASET}.png')
     plt.close()
 
 # Confusion matrix
 def confusion_matrix_dash(y_test, y_pred, selection=None, crossover=None):
     ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
     plt.title(f'{str.upper(selection)}.{str.upper(crossover)}')
-    plt.savefig(f'cm_{selection}.{crossover}.{FILENAME_COMPLEMENT}.png')
+    plt.savefig(f'cm_{selection}.{crossover}.{DATASET}.png')
     plt.close()
 
 # ROC Curve
@@ -59,7 +65,7 @@ def roc_curve_dash(y_test, y_pred, selection=None, crossover=None):
     plt.title(f'{str.upper(selection)}.{str.upper(crossover)}')
     plt.xlabel("False Positive Rate")
     plt.ylabel("True Positive Rate")
-    plt.savefig(f'roc_{selection}.{crossover}.{FILENAME_COMPLEMENT}.png')
+    plt.savefig(f'roc_{selection}.{crossover}.{DATASET}.png')
     plt.close()
 
 # Train test dataset
@@ -68,14 +74,15 @@ def get_train_dataset(x, y):
     return x_train, x_test, y_train, y_test
 
 # Baseline
-scores = cross_val_score(mdl, x, y, cv=5, n_jobs=-1)
-with open(f'baseline_dataset_{FILENAME_COMPLEMENT}.txt', 'w') as file:
-    file.write(f'Score Baseline: {mean(scores)}')
 x_train, x_test, y_train, y_test = get_train_dataset(x, y)
-mdl.fit(x_train, y_train)
-y_pred = mdl.predict(x_test)
-confusion_matrix_dash(y_test, y_pred, 'baseline', 'dataset')
-roc_curve_dash(y_test, y_pred, 'baseline', 'dataset')
+for mdl in models:
+    scores = cross_val_score(mdl[0], x, y, cv=5, n_jobs=-1)
+    with open(f'baseline_dataset_{DATASET}_{mdl[1]}.txt', 'w') as file:
+        file.write(f'Score Baseline: {mean(scores)}')    
+    mdl[0].fit(x_train, y_train)
+    y_pred = mdl[0].predict(x_test)
+    confusion_matrix_dash(y_test, y_pred, 'baseline', 'dataset')
+    roc_curve_dash(y_test, y_pred, 'baseline', 'dataset')
 
 # Running
 runs = [('elitism','one-point'), ('elitism','two-points'), ('elitism','uniform'),
@@ -84,39 +91,40 @@ runs = [('elitism','one-point'), ('elitism','two-points'), ('elitism','uniform')
 
 # GA fixed parameters
 ITERATIONS = 30
-POP_SIZE = 150
+POP_SIZE = 100
 
-for run in runs:
-    # GA parameters
-    SELECTION = run[0]
-    CROSSOVER = run[1] 
+for mdl in models:
+    for run in runs:
+        # GA parameters
+        SELECTION = run[0]
+        CROSSOVER = run[1] 
 
-    # Create genetic object
-    g = GeneticFeatures(
-        qtd_features=x.shape[1], 
-        pop_size=POP_SIZE, 
-        iterations=ITERATIONS,
-        crossover=CROSSOVER,
-        selection=SELECTION, 
-        metric_ascending=True)
+        # Create genetic object
+        g = GeneticFeatures(
+            qtd_features=x.shape[1], 
+            pop_size=POP_SIZE, 
+            iterations=ITERATIONS,
+            crossover=CROSSOVER,
+            selection=SELECTION, 
+            metric_ascending=True)
 
-    # Overwrite fit method
-    def fit(self):
-        for ind in self.pop:
-            if ind[-1] is None:
-                ccols = get_cols(x, ind)
-                scores = cross_val_score(mdl, x[ccols], y, cv=5, n_jobs=-1)
-                ind[-1] = mean(scores)
-    g.fit = MethodType(fit, g)
+        # Overwrite fit method
+        def fit(self):
+            for ind in self.pop:
+                if ind[-1] is None:
+                    ccols = get_cols(x, ind)
+                    scores = cross_val_score(mdl[0], x[ccols], y, cv=5, n_jobs=-1)
+                    ind[-1] = mean(scores)
+        g.fit = MethodType(fit, g)
 
-    # Running GA
-    print(f'{run[0]} - {run[1]}')
-    max_values, best_ind, iters = g.run()
-    chosen_cols = get_cols(x, best_ind)
-    write_output(f'{SELECTION}.{CROSSOVER}.{FILENAME_COMPLEMENT}.txt', iters, chosen_cols, max_values)
-    plot_dash(max_values, SELECTION, CROSSOVER)
-    x_train, x_test, y_train, y_test = get_train_dataset(x[chosen_cols], y)
-    mdl.fit(x_train, y_train)
-    y_pred = mdl.predict(x_test)
-    confusion_matrix_dash(y_test, y_pred, SELECTION, CROSSOVER)
-    roc_curve_dash(y_test, y_pred, SELECTION, CROSSOVER)
+        # Running GA
+        print(f'{run[0]} - {run[1]}')
+        max_values, best_ind, iters = g.run()
+        chosen_cols = get_cols(x, best_ind)
+        write_output(f'{SELECTION}.{CROSSOVER}.{DATASET}.txt', iters, chosen_cols, max_values)
+        plot_dash(max_values, SELECTION, CROSSOVER)
+        x_train, x_test, y_train, y_test = get_train_dataset(x[chosen_cols], y)
+        mdl[0].fit(x_train, y_train)
+        y_pred = mdl[0].predict(x_test)
+        confusion_matrix_dash(y_test, y_pred, SELECTION, CROSSOVER)
+        roc_curve_dash(y_test, y_pred, SELECTION, CROSSOVER)
